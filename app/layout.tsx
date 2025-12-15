ff57eabe3561d09d3ddffcba525783b81870bb1c
Svitlana Shumal <svetlankachajka@gmail.com>
2025-12-15 13:43:30 +0200

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Manrope, Inter } from 'next/font/google';
+import './globals.css';
+// import Header from '@/components/Header/Header';
+
+const geistSans = Manrope({
+  variable: '--font-manrope',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+const geistMono = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['600'],
+});
+
+export const metadata: Metadata = {
+  title: 'RentalCar',
+  description: 'Welcome to rental car',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* <Header /> */}
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Hero from '@/components/Hero/Hero';
+
+export default function Home() {
+  return <main>{/* <Hero /> */}</main>;
+}
